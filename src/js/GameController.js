5bb2adc6f67df1af7347ba78549f14dd4f025298
Anton Lysenko <antonlysenko161@yandex.ru>
2025-02-06 23:31:24 +0300

    Реализовал отрисовку поля

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,10 +1,13 @@
+import themes from "./themes";
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
   }
-
+  
   init() {
+    this.gamePlay.drawUi(themes.prairie);
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
